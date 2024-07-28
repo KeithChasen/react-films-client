22638ba7613a99cc1e28901f4a0a18dc3ff3f15c
keithchasen <keithchasen89@gmail.com>
2024-07-28 22:34:27 +0200

    basic mui AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
-import Button from '@mui/material/Button';
-import { CssBaseline } from '@mui/material';
+import { 
+  CssBaseline, 
+  Box, 
+  AppBar, 
+  Toolbar, 
+  IconButton, 
+  Typography,
+  Button 
+} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function App() {
   return (
     <>
       <CssBaseline /> {/* Drops basic margins of body */}
-      Films DB
-      <Button variant="contained">Hello world</Button>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
     </>
   )
 }
